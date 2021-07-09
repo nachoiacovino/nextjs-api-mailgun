dd1f492fc7ba6001ba9ec487e783cca1aa523b22
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-07-09 18:49:18 +0200

    Cleanup

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,11 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-let API_KEY = 'key-0dc454a132e089f79541cc7333589384';
-let DOMAIN = 'mg.weblime.com';
+let API_KEY = process.env.API_KEY;
+let DOMAIN = process.env.DOMAIN;
 let mailgun = require('mailgun-js')({ apiKey: API_KEY, domain: DOMAIN });
 
 export default (req, res) => {
-  /*   const { name, email, message } = JSON.parse(req.body); */
-
   let reqBody;
   try {
     reqBody = req.body && JSON.parse(req.body);
@@ -13,31 +11,16 @@ export default (req, res) => {
     reqBody = req.body;
   }
 
-  const name = reqBody?.name || "Name";
-  const email = reqBody?.email || "hello@yes.com";
-  const message = reqBody?.message || "pls";
-
-  /*   console.log({ 'req.body': req.body, reqBody, name, email, message }); */
-
-  /*   console.log("req.query", req.query);
-    console.log("req.body", req.body);
-    console.log("req.body.email", req.body.email);
-    console.log('req.bodyjson', reqBody);
-    console.log('req.bodyjson.email', reqBody.email); */
+  const name = reqBody?.name || "There was a problem retrieving the name.";
+  const email = reqBody?.email || "bigeba8@gmail.com";
+  const message = reqBody?.message || "There was a problem retrieving the message.";
 
   const data = {
     from: `${name} <${email}>`,
-    to: 'bigeba8@gmail.com, nacho.iacovino@gmail.com',
+    to: 'bigeba8@gmail.com',
     subject: `New lead from ${name}`,
     text: `Text is: ${message}`
   };
-
-  /*   const data = {
-      from: 'Excited User <me@samples.mailgun.org>',
-      to: 'bigeba8@gmail.com, nacho.iacovino@gmail.com',
-      subject: 'Hello',
-      text: `Text is:`
-    }; */
 
   try {
     mailgun.messages().send(data, (error, body) => {
